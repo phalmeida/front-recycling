d45bcdfcccb550b3de590d4522d204c41608a5b1
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-08-22 22:37:55 -0300

    Type Conversion

---
+++ javascript-introduction/typeConversion.js
@@ -0,0 +1,7 @@
+console.log("Type Conversion");
+
+console.log(parseInt("2") + parseInt("2"));
+console.log("10" / "2");
+console.log("Philipe" / 2); //Not a number or NaN
+console.log(9.3);
+console.log(9,3);
