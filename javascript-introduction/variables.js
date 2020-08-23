ddff53db84f76558319817d9691f40e8f930fbc9
Philipe Allan Almeida <philipe.almeida@outlook.com>
2020-08-22 21:22:15 -0300

    Working with variables.

---
+++ javascript-introduction/variables.js
@@ -0,0 +1,12 @@
+console.log("Working with variables!");
+
+//Js case sensetive
+
+const idade = 26;
+const Idade = 28;
+
+console.log(idade);
+console.log(Idade);
+
+//do not do
+//name = "Philipe"
